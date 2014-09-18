0001def88e23be28a2109b238e1bcc4f0aeb927d
olzhang <oliver.zhang2718@gmail.com>
2014-09-17 23:29:55 -0700

    created a model for BlogPost and Comments and started on frontend for a blog management web app

--- backend-site/blog-backend/models/blog-post-model.js
+++ backend-site/blog-backend/models/blog-post-model.js
@@ -12,12 +12,14 @@ var mongoose = require("mongoose")
 
 var blogPostObject = {
 		_id: ObjectId, 
+		title: String
 		image: [imageSchema], //subdoc of image
         body: String,
         date: Date,
+        author: String,
         category: String,
         tags: [String],
-        comments: [ObjectId] //Id of comments 
+        comments: [{type: ObjectId, default: null}] //Id of comments 
 }
 
 var imageSchemaObject = {
@@ -27,6 +29,12 @@ var imageSchemaObject = {
     relBase: String
 }
 
+var imageSchema = mongoose.Schema(imageSchemaObject);
+
+imageSchema.virtual('path').get(function() {
+	return relBase+ObjectId.toString();
+})
+
 
     
-module.exports = mongoose.model('BlogModel', mongoose.Schema(blogPostObject));
+module.exports = mongoose.model('BlogPost', mongoose.Schema(blogPostObject));
