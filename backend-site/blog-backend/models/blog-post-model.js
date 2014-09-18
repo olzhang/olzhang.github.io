c110e81278156a10260a454fd8454735c1efb574
Oliver <oliver.zhang2718@gmail.com>
2014-09-17 19:52:02 -0700

    some changes to blog post model

--- backend-site/blog-backend/models/blog-post-model.js
+++ backend-site/blog-backend/models/blog-post-model.js
@@ -3,13 +3,30 @@
  * contains _id, image, body, date, comments, category,
  * tag, and blog post number
  */
-
+ 
 var mongoose = require("mongoose")
 
-var blogPostObject = {
 
-		_id: String,
-		media: String,
-		isDead: {type: Boolean, default: false},
-		isEvil: String
-	}
+// The image will be uploaded via formidable and then put into the mongo server via
+// mongoose-file
+
+var blogPostObject = {
+		_id: ObjectId, 
+		image: [imageSchema], //subdoc of image
+        body: String,
+        date: Date,
+        category: String,
+        tags: [String],
+        comments: [ObjectId] //Id of comments 
+}
+
+var imageSchemaObject = {
+    name: String,
+    caption: String,
+    blog: ObjectId,
+    relBase: String
+}
+
+
+    
+module.exports = mongoose.model('BlogModel', mongoose.Schema(blogPostObject));
