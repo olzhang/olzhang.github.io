1022022404f81712fe6ec05b1a569581c49e06a0
Oliver <oliver.zhang2718@gmail.com>
2014-10-04 13:38:30 -0700

    - removed the blog portion of the site and placed it into to a different project - moved the img, js, css folder into /assets - updated the references to /assets - created an "about me" page - added a headshot

--- backend-site/server.js
+++ backend-site/server.js
@@ -2,9 +2,8 @@ var path = require('path')
 var express = require('express')
 var app = express()
 
-app.use(express.static('../frontend-site/'))i;
+
+app.use(express.static("../frontend-site"));
 
 app.listen(8080)
-
-
 
